37b44e549b8d45d02718b399daf43ebfad595c2c
Yadhunadh Chemmarassery <yadhu680@gmail.com>
2025-10-14 19:05:17 +0530

    issue fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+/** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  output: "export",
-  basePath: isProd ? "/saas-dashboard" : "",
-  assetPrefix: isProd ? "/saas-dashboard/" : "",
+  output: 'export', // enable static export
+  basePath: isProd ? '/saas-dashboard' : '', // your repo name
+  assetPrefix: isProd ? '/saas-dashboard/' : '', // ensures _next files are served correctly
+  reactStrictMode: true,
 };
